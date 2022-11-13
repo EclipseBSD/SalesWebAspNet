9c5584817e6899debe0eea1764620d4d35ff1cd2
EclipseBSD <eclipsebsdcontact@protonmail.com>
2022-11-13 11:00:46 -0400

    CRUD Scaffolding

---
+++ SaleWebAspNet/Data/SaleWebAspNetContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SaleWebAspNet.Models;
+
+namespace SaleWebAspNet.Data
+{
+    public class SaleWebAspNetContext : DbContext
+    {
+        public SaleWebAspNetContext (DbContextOptions<SaleWebAspNetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SaleWebAspNet.Models.Department> Department { get; set; }
+    }
+}
