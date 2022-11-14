551dd19b5a77af8d2c9793124831d7ce2e474a11
EclipseBSD <eclipsebsdcontact@protonmail.com>
2022-11-14 09:29:36 -0400

    SellerService and basic FindAll

---
+++ SaleWebAspNet/Services/SellerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SaleWebAspNet.Data;
+using SaleWebAspNet.Models;
+
+namespace SaleWebAspNet.Services
+{
+    public class SellerService
+    {
+        private readonly SaleWebAspNetContext _context;
+
+        public SellerService(SaleWebAspNetContext context)
+        {
+            _context = context;
+        }
+
+        //Implementação para retornar todos os vendedores em uma lista.
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
