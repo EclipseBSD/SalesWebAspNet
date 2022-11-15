864489c9e379e8bce6dfcc2cb70dafe849292ba7
EclipseBSD <eclipsebsdcontact@protonmail.com>
2022-11-15 10:09:36 -0400

    Validation

--- SaleWebAspNet/Models/Seller.cs
+++ SaleWebAspNet/Models/Seller.cs
@@ -8,19 +8,31 @@ namespace SaleWebAspNet.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")] //Atributo para definir o que vai ser mostrado na aplicação
         [DataType(DataType.Date)] //Atributo para definir o tipo de data na view
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")] //Formatando data para formato brasileiro
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")] //Formatação do atributo com duas casa decimais
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         [Display(Name = "Department")]
+
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>(); //Associação com ICollection
 
